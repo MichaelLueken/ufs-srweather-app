76a181b8d6887e0f83ae47eedbe6cef5934768b8
michael.lueken <Michael.Lueken@noaa.gov>
2025-10-29 12:54:34 -0400

    [feature/aqmv8_p1.2] Updated: * Externals.cfg - Replace NOAA-EPIC fork with authoritative repository for UFS_UTILS (hash 6c05801) * scripts/exregional_make_orog.sh - Apply necessary changes to allow make_orog task to pass following update to HEAD of authoritative UFS_UTILS repository * sorc/CMakeLists.txt - Compile chgres_cube with necessary libraries for regional

--- scripts/exregional_make_orog.sh
+++ scripts/exregional_make_orog.sh
@@ -182,9 +182,9 @@ cd "${DATA}"
 # Copy topography and related data files from the system directory (FIXorg)
 # to the temporary directory.
 #
-cp ${FIXorg}/thirty.second.antarctic.new.bin fort.15
-cp ${FIXorg}/landcover30.fixed .
-cp ${FIXorg}/gmted2010.30sec.int fort.235
+cp ${FIXorg}/topography.antarctica.ramp.30s.nc .
+cp ${FIXorg}/landcover.umd.30s.nc .
+cp ${FIXorg}/topography.gmted2010.30s.nc .
 #
 #-----------------------------------------------------------------------
 #
@@ -209,26 +209,26 @@ grid_fp="${FIXlam}/${grid_fn}"
 #
 #-----------------------------------------------------------------------
 #
-mtnres=1
-lonb=0
-latb=0
-jcap=0
-NR=0
-NF1=0
-NF2=0
-efac=0
-blat=0
+#mtnres=1
+#lonb=0
+#latb=0
+#jcap=0
+#NR=0
+#NF1=0
+#NF2=0
+#efac=0
+#blat=0
 
 input_redirect_fn="INPS"
 orogfile="none"
 
-echo $mtnres $lonb $latb $jcap $NR $NF1 $NF2 $efac $blat > "${input_redirect_fn}"
+#echo $mtnres $lonb $latb $jcap $NR $NF1 $NF2 $efac $blat > "${input_redirect_fn}"
 #
 # The following two inputs are read in as strings, so they must be quoted
 # in the input file.
 #
 echo "\"${grid_fp}\"" >> "${input_redirect_fn}"
-echo "\"$orogfile\"" >> "${input_redirect_fn}"
+#echo "\"$orogfile\"" >> "${input_redirect_fn}"
 echo ".false." >> "${input_redirect_fn}" #MASK_ONLY
 echo "none" >> "${input_redirect_fn}" #MERGE_FILE
 cat "${input_redirect_fn}"
